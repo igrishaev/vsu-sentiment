f07365f251d9af14259d61575efc465a181af5be
Ivan Grishaev <ivan@grishaev.me>
2016-09-01 14:01:30 +0300

    fisher algo updated

--- src/sentiment.py
+++ src/sentiment.py
@@ -1,6 +1,5 @@
 
 import math
-import pprint
 import csv
 import re
 import json
@@ -74,6 +73,19 @@ def parse_category(category_str):
     return CATEGORY.UNKNOWN
 
 
+def ichain(obj, *items):
+
+    def get_item(obj, item):
+        if obj is None:
+            return None
+        try:
+            return obj[item]
+        except:
+            return None
+
+    return reduce(get_item, items, obj)
+
+
 def populate_state(state):
 
     with open('source.csv', 'rb') as f:
@@ -96,11 +108,11 @@ def populate_state(state):
 
 
 def f_count(state, feature, category):
-    return state['feature'][feature][category]
+    return ichain(state, 'feature', feature, category) or 0
 
 
 def c_count(state, category):
-    return state['category'][category]
+    return ichain(state, 'category', category) or 0
 
 
 def div(a, b):
@@ -111,35 +123,27 @@ def div(a, b):
 
 
 def p_feature_category(state, feature, category):
-    a = f_count(state, feature, category)
-    b = float(c_count(state, category))
-    return div(a, b)
+    return div(
+        f_count(state, feature, category),
+        c_count(state, category)
+    )
 
 
 def get_categories(state):
     return state['category'].keys()
 
 
-def p_feature_category_w(state, feature, category, weight=1, assump=0.5):
-    prob = p_feature_category(state, feature, category)
-
-    cats = get_categories(state)
-    counts = [f_count(state, feature, cat) for cat in cats]
-    print counts
-    print cats
-    totals = sum(counts)
-
-    a = weight * assump + totals * prob
-    b = weight + totals
-
-    return div(a, b)
+# def p_feature_category_w(state, feature, category, weight=1, assump=0.5):
+#     prob = p_feature_category(state, feature, category)
+#     cats = get_categories(state)
+#     counts = [f_count(state, feature, cat) for cat in cats]
+#     totals = sum(counts)
+#     a = weight * assump + totals * prob
+#     b = weight + totals
+#     return div(a, b)
 
 
-def pretty_print(state):
-    pprint.pprint(state)
-
-
-def p_fisher(state, feature, category):
+def p_fisher_prob(state, feature, category):
 
     prob = p_feature_category(state, feature, category)
 
@@ -150,22 +154,23 @@ def p_fisher(state, feature, category):
     return div(prob, sum(prob_list))
 
 
-def invchi2(chi, df):
-    m = chi / 2.0
-    sum = term = math.exp(-m)
-    for i in range(1, df//2):
-        term *= m / i
-        sum += term
-    return min(sum, 1.0)
+# def invchi2(chi, df):
+#     m = chi / 2.0
+#     sum = term = math.exp(-m)
+#     for i in range(1, df//2):
+#         term *= m / i
+#         sum += term
+#     return min(sum, 1.0)
 
 
 def p_fisher_item(state, text, category):
+
     feature_list = get_features(text)
     if not feature_list:
         return 0
 
     prob_list = [
-        p_feature_category(state, feature, category)
+        p_fisher_prob(state, feature, category)
         for feature in feature_list
 
     ]
@@ -176,8 +181,20 @@ def p_fisher_item(state, text, category):
 
     return mult
 
-    score = -2 * math.log(mult)
-    return invchi2(score, len(feature_list) * 2)
+    # score = -2 * math.log(mult)
+    # return invchi2(score, len(feature_list) * 2)
+
+
+def get_text_category(state, text):
+    category_list = get_categories(state)
+
+    prob_list = [p_fisher_item(state, text, cat)
+                 for cat in category_list]
+
+    pairs = zip(category_list, prob_list)
+    pairs.sort(key=(lambda (cat, prob): -prob))
+
+    return pairs[0][0]
 
 
 def main():
@@ -188,15 +205,24 @@ def main():
 
     state = state_load()
 
+    for text in (
+            'I love you',
+            'I hate you',
+            'The movie was pretty good',
+            'The movie was quite boring',
+
+    ):
+        print text, ' -- ', get_text_category(state, text)
+
     # pretty_print(state)
     # print p_feature_category_w(state, 'love', True)
     # print p_fisher(state, 'fuck', CATEGORY.NEGATIVE)
 
-    print p_fisher_item(state, 'I hate you', CATEGORY.NEGATIVE)
-    print p_fisher_item(state, 'I hate you', CATEGORY.POSITIVE)
+    # print p_fisher_item(state, 'I hate you', CATEGORY.NEGATIVE)
+    # print p_fisher_item(state, 'I hate you', CATEGORY.POSITIVE)
 
-    print p_fisher_item(state, 'I love you', CATEGORY.NEGATIVE)
-    print p_fisher_item(state, 'I love you', CATEGORY.POSITIVE)
+    # print p_fisher_item(state, 'I love you', CATEGORY.NEGATIVE)
+    # print p_fisher_item(state, 'I love you', CATEGORY.POSITIVE)
 
     # print p_fisher_item(state, 'kiss her hug her', CATEGORY.NEGATIVE)
     # print p_fisher_item(state, 'kiss her hug her', CATEGORY.POSITIVE)
